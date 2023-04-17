6671d27349bdeef58c0d9af9c438d9ea04b3d960
Ivan <lifefire@inbox.ru>
2023-04-17 21:18:57 +0300

    step slider slick

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left_solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right_solid.png"></button>'
+    });
+});
